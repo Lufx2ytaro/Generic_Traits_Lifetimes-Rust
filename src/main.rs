17ced502ffa5ffa2cca820fee761348f9c163618
Lufx2ytaro <kunticha09@gmail.com>
2025-03-20 19:29:36 +0300

    Add right code

--- src/main.rs
+++ src/main.rs
@@ -35,3 +35,29 @@
 // }
 ///////////////////////Это ошибка использовать функцию с тем же кодом в двух местах///////////////////////////////
 
+// fn largest<T>(list: &[T]) -> &T {
+//   let mut largest = &list[0];
+
+//   for item in list {
+//       if item > largest {
+//           largest = item;
+//       }
+//   }
+
+//   largest
+// }
+
+// fn main() {
+//   let number_list = vec![34, 50, 25, 100, 65];
+
+//   let result = largest(&number_list);
+//   println!("The largest number is {result}");
+
+//   let char_list = vec!['y', 'm', 'a', 'q'];
+
+//   let result = largest(&char_list);
+//   println!("The largest char is {result}");
+// }
+
+///Это вот пример работы правльного кода. Он заработает если добавить в <T ... > ----> :std::cmp::PartialOrd /////////////////
+ 
